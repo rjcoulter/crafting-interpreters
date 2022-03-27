4a248ab9000f9cec1616a0f37205e78ad79f067d
rjcoulter <ryancoulter22@gmail.com>
2022-03-27 11:00:45 -0400

    Add support for runtime errors in REPL shell

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 public class Lox {
 
-    static boolean hadError;
+    private static final Interpreter interpreter = new Interpreter();
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if(hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
     
     private static void runPrompt() throws IOException {
@@ -51,6 +54,8 @@ public class Lox {
 
         // Stop if there was a syntax error.
         if (hadError) return;
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -68,5 +73,10 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
