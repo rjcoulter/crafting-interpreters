7c9c3032c981e1d4d104de089302647817361308
rjcoulter <ryancoulter22@gmail.com>
2022-04-06 08:18:31 -0400

    Add chunks

--- clox/chunk.h
+++ clox/chunk.h
@@ -7,19 +7,20 @@
 
 typedef enum {
   OP_RETURN,
-    OP_CONSTANT,
+  OP_CONSTANT,
 } OpCode;
 
 typedef struct {
   int count;
   int capacity;
   uint8_t* code;
+  int* lines;
   ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif
